143bb451afd342783c9fda12010604635a9d1fee
HassenH1 <hasansaid51@gmail.com>
2020-09-15 22:10:40 -0700

    ...

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
-import { doCreateUserWithEmailAndPassword } from '../../firebase/users'
-import {withRouter} from 'react-router-dom'
-import { Div } from './styled'
-
+import React, { Component } from "react";
+import { doCreateUserWithEmailAndPassword } from "../../firebase/users";
+import { withRouter } from "react-router-dom";
+import { Div, InputField, InputBtn } from "./styled";
 
 class index extends Component {
   state = {
@@ -10,55 +9,123 @@ class index extends Component {
     password: "",
     email: "",
     session: {},
-    message: ""
-  }
+    message: "",
+  };
   /////////////////////////////////////////need session and error message
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-    console.log(this.state)
-  }
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state);
+  };
   handleSubmit = (e) => {
-    const { email, password } = this.state
-    e.preventDefault()
-    console.log("hitting here")
+    const { email, password } = this.state;
+    e.preventDefault();
+    console.log("hitting here");
     doCreateUserWithEmailAndPassword(email, password)
-      .then(async authUser => {
+      .then(async (authUser) => {
         const user = {
           username: this.state.username,
           email: this.state.email,
-          _id: authUser.user.uid
-        }
-        console.log(authUser)
-        const createdUser = await fetch('/auth/register', {
+          _id: authUser.user.uid,
+        };
+        console.log(authUser);
+        const createdUser = await fetch("/auth/register", {
           method: "POST",
           body: JSON.stringify(user),
           headers: {
-            "Content-Type": "application/json"
-          }
-        })
-        const createdUserJson = await createdUser.json()
-        this.props.doSetCurrentUser(createdUserJson)
-        this.props.history.push('/')
+            "Content-Type": "application/json",
+          },
+        });
+        const createdUserJson = await createdUser.json();
+        this.props.doSetCurrentUser(createdUserJson);
+        this.props.history.push("/");
       })
-      .catch(error => {
-        this.setState({ error })
-      })
-  }
+      .catch((error) => {
+        this.setState({ error });
+      });
+  };
   render() {
     return (
       <Div>
-        <form onSubmit={this.handleSubmit}>
-          <h4>Register</h4>
-          <input type="email" name="email" onChange={this.handleChange} placeholder="email"/>
-          <input type="username" name="username" onChange={this.handleChange} placeholder="username"/>
-          <input type="password" name="password" onChange={this.handleChange} placeholder="password"/>
-          <input type='submit' value="Submit" />
-          {this.state.error && <p style={{'color': 'red'}}>{this.state.error.message}</p>}
+        <div
+          style={{
+            height: "50vh",
+            border: "1px solid whitesmoke",
+            width: "30vw",
+          }}
+        >
+          <form
+            onSubmit={this.handleSubmit}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              background: "#ffffff",
+              height: "100%",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              alignContent: "center",
+              width: "auto",
+            }}
+          >
+            <div>
+              <h4>Register</h4>
+            </div>
 
-        </form>
+            <div
+              style={
+                {
+                  /*border: "1px solid red"*/
+                }
+              }
+            >
+              <InputField
+                type="email"
+                name="email"
+                onChange={this.handleChange}
+                placeholder="email"
+              />
+            </div>
+
+            <div>
+              {/* <InputField
+                type="username"
+                name="username"
+                onChange={this.handleChange}
+                placeholder="username"
+              /> */}
+            </div>
+
+            <div>
+              {/* <InputField
+                type="password"
+                name="password"
+                onChange={this.handleChange}
+                placeholder="password"
+              /> */}
+            </div>
+
+            <div>
+              <InputBtn type="submit" value="Submit" />
+            </div>
+
+            <div>
+              {this.state.error && (
+                <p style={{ color: "red" }}>{this.state.error.message}</p>
+              )}
+            </div>
+          </form>
+        </div>
+
+        <div style={{ height: "50vh", width: "30vw" }}>
+          <img
+            src="https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+            alt="img"
+            height="100%"
+            width="100%"
+          />
+        </div>
       </Div>
-    )
+    );
   }
 }
 
-export default withRouter(index)
+export default withRouter(index);
--- src/components/Register/styled.js
+++ src/components/Register/styled.js
@@ -1,11 +1,46 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+  height: 100vh;
+`;
+export const oldDiv = styled.div`
   width: 900px;
-  height: 300px;
+  height: 30vh;
   position: absolute;
   left: 20%;
   top: 30%;
   text-align: center;
   z-index: 1px;
-`
+  border: 1px solid red;
+`;
+
+export const InputField = styled.input`
+  padding: 12px 20px;
+  margin: 8px 0;
+  boxsizing: border-box;
+  &:focus {
+    border: 3px solid #9ecaed;
+    box-shadow: 0 0 10px #9ecaed;
+  }
+`;
+
+export const InputBtn = styled.input`
+  width: 10rem;
+  height: 3rem;
+  transition-duration: 0.4s;
+  font-size: 18px;
+  background-color: #ff7b00;
+  color: white;
+  border: 0.6px solid #ff7b00;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  &:hover {
+    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+      0 17px 50px 0 rgba(0, 0, 0, 0.19);
+    color: #ff7b00;
+    background-color: white;
+  }
+`;
