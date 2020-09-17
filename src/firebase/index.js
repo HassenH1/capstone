b648c9d493113df3aaa2e40cc2d9109f197aa1c6
HassenH1 <hasansaid51@gmail.com>
2020-09-16 19:49:42 -0700

    ..

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,15 @@
-import app from 'firebase/app'
-import 'firebase/storage'
+import app from "firebase/app";
+import "firebase/storage";
 
-const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
+// Initialize Firebase
+const firebase = app.initializeApp(firebaseConfig);
 
-const firebase = app.initializeApp(config)
-
-export default firebase
+export default firebase;
