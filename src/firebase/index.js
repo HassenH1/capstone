7aa0c47900a2719b2d76899869946a458d69bad2
HassenH1 <hasansaid51@gmail.com>
2020-09-16 21:17:43 -0700

    .

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,14 +2,21 @@ import app from "firebase/app";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyColdT9rJEuFe4ACjVHyqHdFi_Uyq7gVkI",
+  authDomain: "e-commerce-6a142.firebaseapp.com",
+  databaseURL: "https://e-commerce-6a142.firebaseio.com",
+  projectId: "e-commerce-6a142",
+  storageBucket: "e-commerce-6a142.appspot.com",
+  messagingSenderId: 798494773585,
 };
 // Initialize Firebase
 const firebase = app.initializeApp(firebaseConfig);
 
 export default firebase;
+
+// REACT_APP_API_KEY =
+// REACT_APP_AUTH_DOMAIN = "e-commerce-6a142.firebaseapp.com"
+// REACT_APP_DATABASE_URL = "https://e-commerce-6a142.firebaseio.com"
+// REACT_APP_PROJECT_ID = "e-commerce-6a142"
+// REACT_APP_STORAGE_BUCKET = "e-commerce-6a142.appspot.com"
+// REACT_APP_MESSAGING_SENDER_ID = 798494773585
