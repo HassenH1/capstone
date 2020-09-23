085dd74f1dbe0b5d9846f66cf7f3e7903e977b29
HassenH1 <hasansaid51@gmail.com>
2020-09-22 17:44:57 -0700

    ...

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,15 +2,24 @@ import app from "firebase/app";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyColdT9rJEuFe4ACjVHyqHdFi_Uyq7gVkI",
+  authDomain: "e-commerce-6a142.firebaseapp.com",
+  databaseURL: "https://e-commerce-6a142.firebaseio.com",
+  projectId: "e-commerce-6a142",
+  storageBucket: "e-commerce-6a142.appspot.com",
+  messagingSenderId: 798494773585,
+  appId: "1:798494773585:web:86a644f2c30686a038c370",
+  measurementId: "G-S91NSZKJ0Z",
 };
+
+// REACT_APP_API_KEY=AIzaSyColdT9rJEuFe4ACjVHyqHdFi_Uyq7gVkI
+// REACT_APP_AUTH_DOMAIN=e-commerce-6a142.firebaseapp.com
+// REACT_APP_DATABASE_URL=https://e-commerce-6a142.firebaseio.com
+// REACT_APP_PROJECT_ID=e-commerce-6a142
+// REACT_APP_STORAGE_BUCKET=e-commerce-6a142.appspot.com
+// REACT_APP_MESSAGING_SENDER_ID=798494773585
+// REACT_APP_APP_ID=1:798494773585:web:86a644f2c30686a038c370
+// REACT_APP_MEASUREMENT_ID=G-S91NSZKJ0Z
 
 // Initialize Firebase
 const firebase = app.initializeApp(firebaseConfig);
